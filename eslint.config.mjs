38a5d8c922c8cd6ce749ca1446114add4053d1ac
Abhi Agarwal <abhiaagarwal01@gmail.com>
2025-04-22 13:11:11 -0400

    Create landing page
    Signed-off-by: Abhi Agarwal <abhiaagarwal01@gmail.com>

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginAstro from "eslint-plugin-astro";
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.strictTypeChecked,
+    tseslint.configs.stylisticTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    },
+    ...eslintPluginAstro.configs.recommended,
+);
