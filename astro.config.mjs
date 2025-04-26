b648d8c38ba1fe8b1be8675f76027b94b15442e8
Abhi Agarwal <abhiaagarwal01@gmail.com>
2025-04-25 21:52:57 -0400

    Add view transitions
    Signed-off-by: Abhi Agarwal <abhiaagarwal01@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import { visualizer } from "rollup-plugin-visualizer";
 
 import sitemap from "@astrojs/sitemap";
-
 import preact from "@astrojs/preact";
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://abhi.rodeo",
     prefetch: true,
+    trailingSlash: "never",
     vite: {
         plugins: [tailwindcss()],
     },
-    integrations: [sitemap(), preact()],
+    integrations: [sitemap(), preact(), compress()],
 });
